8b7c9a38e66641cc58d577a43049e12e82b2329c
kuviman <kuviman@gmail.com>
2020-07-21 10:09:00 +0400

    Implement events for web

--- core/src/window/events/web.rs
+++ core/src/window/events/web.rs
@@ -102,142 +102,124 @@ impl Convert<i16> for MouseButton {
     }
 }
 
-// impl Convert<we::KeyDownEvent> for Event {
-//     fn convert(event: we::KeyDownEvent) -> Option<Event> {
-//         Convert::convert(event.code()).map(|key| Event::KeyDown { key })
-//     }
-// }
+impl Convert<web_sys::KeyboardEvent> for Event {
+    fn convert(event: web_sys::KeyboardEvent) -> Option<Event> {
+        if event.repeat() {
+            return None;
+        }
+        let key = Convert::convert(event.code())?;
+        Some(match event.type_().as_str() {
+            "keydown" => Event::KeyDown { key },
+            "keyup" => Event::KeyUp { key },
+            _ => return None,
+        })
+    }
+}
 
-// impl Convert<we::KeyUpEvent> for Event {
-//     fn convert(event: we::KeyUpEvent) -> Option<Event> {
-//         Convert::convert(event.code()).map(|key| Event::KeyUp { key })
-//     }
-// }
+impl Convert<web_sys::MouseEvent> for Event {
+    fn convert(event: web_sys::MouseEvent) -> Option<Event> {
+        let button = Convert::convert(event.button());
+        let canvas: web_sys::HtmlCanvasElement = event.target().unwrap().dyn_into().unwrap();
+        let position = vec2(
+            event.offset_x(),
+            canvas.height() as i32 - 1 - event.offset_y(),
+        )
+        .map(|x| x as f64);
+        Some(match event.type_().as_str() {
+            "mousedown" => Event::MouseDown {
+                position,
+                button: button?,
+            },
+            "mouseup" => Event::MouseUp {
+                position,
+                button: button?,
+            },
+            "mousemove" => Event::MouseMove { position },
+            _ => return None,
+        })
+    }
+}
 
-// fn convert_position<E: we::IMouseEvent>(event: E) -> Vec2<f64> {
-//     let canvas: stdweb::web::html_element::CanvasElement =
-//         stdweb::unstable::TryInto::try_into(event.target().unwrap()).unwrap();
-//     vec2(
-//         event.offset_x(),
-//         canvas.height() as f64 - 1.0 - event.offset_y(),
-//     )
-// }
+impl Convert<web_sys::WheelEvent> for Event {
+    fn convert(event: web_sys::WheelEvent) -> Option<Event> {
+        Some(Event::Wheel {
+            delta: -event.delta_y()
+                * match event.delta_mode() {
+                    web_sys::WheelEvent::DOM_DELTA_PIXEL => 1.0,
+                    web_sys::WheelEvent::DOM_DELTA_LINE => 51.0,
+                    web_sys::WheelEvent::DOM_DELTA_PAGE => 800.0,
+                    _ => {
+                        error!("Unexpected delta mode: {}", event.delta_mode());
+                        return None;
+                    }
+                },
+        })
+    }
+}
 
-// impl Convert<we::MouseDownEvent> for Event {
-//     fn convert(event: we::MouseDownEvent) -> Option<Event> {
-//         Convert::convert(event.button()).map(|button| Event::MouseDown {
-//             position: convert_position(event),
-//             button,
-//         })
-//     }
-// }
-
-// impl Convert<we::MouseUpEvent> for Event {
-//     fn convert(event: we::MouseUpEvent) -> Option<Event> {
-//         Convert::convert(event.button()).map(|button| Event::MouseUp {
-//             position: convert_position(event),
-//             button,
-//         })
-//     }
-// }
-
-// impl Convert<we::MouseMoveEvent> for Event {
-//     fn convert(event: we::MouseMoveEvent) -> Option<Event> {
-//         Some(Event::MouseMove {
-//             position: convert_position(event),
-//         })
-//     }
-// }
-
-// impl Convert<we::MouseWheelEvent> for Event {
-//     fn convert(event: we::MouseWheelEvent) -> Option<Event> {
-//         Some(Event::Wheel {
-//             delta: -event.delta_y()
-//                 * match event.delta_mode() {
-//                     we::MouseWheelDeltaMode::Pixel => 1.0,
-//                     we::MouseWheelDeltaMode::Line => 51.0,
-//                     we::MouseWheelDeltaMode::Page => 800.0,
-//                 },
-//         })
-//     }
-// }
-
-// fn convert_touch(touch: stdweb::web::Touch) -> TouchPoint {
-//     use stdweb::web::IHtmlElement;
-//     let canvas: stdweb::web::html_element::CanvasElement =
-//         stdweb::unstable::TryInto::try_into(touch.target()).unwrap();
-//     let rect = canvas.get_bounding_client_rect();
-//     let offset_x = touch.page_x() - rect.get_left();
-//     let offset_y = touch.page_y() - rect.get_top();
-//     TouchPoint {
-//         position: vec2(offset_x, canvas.height() as f64 - 1.0 - offset_y),
-//     }
-// }
-
-// impl Convert<we::TouchStart> for Event {
-//     fn convert(event: we::TouchStart) -> Option<Event> {
-//         use stdweb::web::event::ITouchEvent;
-//         Some(Event::TouchStart {
-//             touches: event.touches().into_iter().map(convert_touch).collect(),
-//         })
-//     }
-// }
-
-// impl Convert<we::TouchMove> for Event {
-//     fn convert(event: we::TouchMove) -> Option<Event> {
-//         use stdweb::web::event::ITouchEvent;
-//         Some(Event::TouchMove {
-//             touches: event.touches().into_iter().map(convert_touch).collect(),
-//         })
-//     }
-// }
-
-// impl Convert<we::TouchEnd> for Event {
-//     fn convert(event: we::TouchEnd) -> Option<Event> {
-//         Some(Event::TouchEnd)
-//     }
-// }
-
-// impl Convert<we::TouchCancel> for Event {
-//     fn convert(event: we::TouchCancel) -> Option<Event> {
-//         Some(Event::TouchEnd)
-//     }
-// }
+impl Convert<web_sys::TouchEvent> for Event {
+    fn convert(event: web_sys::TouchEvent) -> Option<Event> {
+        let canvas: web_sys::HtmlCanvasElement = event.target().unwrap().dyn_into().unwrap();
+        let rect = canvas.get_bounding_client_rect();
+        let touches = event.touches();
+        let touches = (0..touches.length())
+            .map(|index| {
+                let touch = touches.item(index).unwrap();
+                let offset_x = touch.page_x() as f64 - rect.left();
+                let offset_y = touch.page_y() as f64 - rect.top();
+                TouchPoint {
+                    position: vec2(offset_x, canvas.height() as f64 - 1.0 - offset_y),
+                }
+            })
+            .collect();
+        Some(match event.type_().as_str() {
+            "touchstart" => Event::TouchStart { touches },
+            "touchmove" => Event::TouchMove { touches },
+            "touchcancel" | "touchend" => Event::TouchEnd,
+            _ => return None,
+        })
+    }
+}
 
 impl Window {
+    fn subscribe_to<T, F>(&self, handler: &Rc<F>, event_name: &str)
+    where
+        T: wasm_bindgen::convert::FromWasmAbi + 'static,
+        T: AsRef<web_sys::Event>,
+        T: Clone,
+        Event: Convert<T>,
+        F: Fn(Event) + 'static,
+    {
+        let handler = handler.clone();
+        let canvas = self.canvas.clone();
+        let handler = move |event: T| {
+            canvas.focus();
+            if event.as_ref().type_() == "contextmenu" {
+                event.as_ref().prevent_default();
+            }
+            if let Some(e) = Convert::convert(event.clone()) {
+                handler(e);
+                event.as_ref().prevent_default();
+            }
+        };
+        let handler = wasm_bindgen::closure::Closure::wrap(Box::new(handler) as Box<dyn Fn(T)>);
+        self.canvas
+            .add_event_listener_with_callback(event_name, handler.as_ref().unchecked_ref());
+        handler.forget(); // TODO: not forget
+    }
     pub(crate) fn subscribe_events<F: Fn(Event) + 'static>(&self, handler: F) {
         let handler = Rc::new(handler);
-        macro_rules! setup_event {
-            ($canvas:expr, $handler:expr, $event:ty) => {
-                let handler = $handler.clone();
-                let canvas = $canvas;
-                let event_handler = {
-                    let canvas = canvas.clone();
-                    move |event: $event| {
-                        canvas_clone.focus();
-                        if let Some(e) = Convert::convert(event.clone()) {
-                            handler(e);
-                            event.prevent_default();
-                        }
-                    }
-                };
-                $canvas.add_event_listener(move |event: $event| {});
-            };
-        }
-        // setup_event!(self.canvas, handler, web_sys::KeyEvent);
-        // setup_event!(self.canvas, handler, we::KeyUpEvent);
-        // setup_event!(self.canvas, handler, we::MouseDownEvent);
-        // setup_event!(self.canvas, handler, we::MouseUpEvent);
-        // setup_event!(self.canvas, handler, we::MouseMoveEvent);
-        // setup_event!(self.canvas, handler, we::MouseWheelEvent);
-        // setup_event!(self.canvas, handler, we::TouchStart);
-        // setup_event!(self.canvas, handler, we::TouchMove);
-        // setup_event!(self.canvas, handler, we::TouchEnd);
-        // setup_event!(self.canvas, handler, we::TouchCancel);
-
-        // self.canvas
-        //     .add_event_listener(move |event: we::ContextMenuEvent| {
-        //         event.prevent_default();
-        //     });
+        let handler = &handler;
+        self.subscribe_to::<web_sys::KeyboardEvent, _>(handler, "keydown");
+        self.subscribe_to::<web_sys::KeyboardEvent, _>(handler, "keyup");
+        self.subscribe_to::<web_sys::MouseEvent, _>(handler, "mousedown");
+        self.subscribe_to::<web_sys::MouseEvent, _>(handler, "mouseup");
+        self.subscribe_to::<web_sys::MouseEvent, _>(handler, "mousemove");
+        self.subscribe_to::<web_sys::WheelEvent, _>(handler, "wheel");
+        self.subscribe_to::<web_sys::TouchEvent, _>(handler, "touchstart");
+        self.subscribe_to::<web_sys::TouchEvent, _>(handler, "touchmove");
+        self.subscribe_to::<web_sys::TouchEvent, _>(handler, "touchend");
+        self.subscribe_to::<web_sys::TouchEvent, _>(handler, "touchcancel");
+        self.subscribe_to::<web_sys::MouseEvent, _>(handler, "contextmenu");
     }
 }
