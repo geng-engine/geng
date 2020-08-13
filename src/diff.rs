a3df3b7911069f0f37521091e9bbbb6b02a85d13
kuviman <kuviman@gmail.com>
2020-08-13 12:27:19 +0400

    Improve Diff trait

--- src/diff.rs
+++ src/diff.rs
@@ -1,9 +1,22 @@
 use super::*;
 
 pub trait Diff:
-    Serialize + for<'de> Deserialize<'de> + Trans + Sync + Send + Clone + 'static
+    Serialize + for<'de> Deserialize<'de> + Trans + Sync + Send + Clone + PartialEq + 'static
 {
     type Delta: Serialize + for<'de> Deserialize<'de> + Trans + Sync + Send + Clone + 'static;
     fn diff(&self, to: &Self) -> Self::Delta;
     fn update(&mut self, delta: &Self::Delta);
+}
+
+impl<
+        T: Serialize + for<'de> Deserialize<'de> + Trans + Sync + Send + Copy + PartialEq + 'static,
+    > Diff for T
+{
+    type Delta = Self;
+    fn diff(&self, to: &Self) -> Self {
+        *to
+    }
+    fn update(&mut self, new_value: &Self) {
+        *self = *new_value;
+    }
 }
