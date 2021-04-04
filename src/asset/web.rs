e2876aae4655733fb476e05c5fbe3abeacc36c4d
kuviman <kuviman@gmail.com>
2021-04-04 14:34:45 +0400

    Fix ios audio loading

--- src/asset/web.rs
+++ src/asset/web.rs
@@ -66,6 +66,7 @@ impl LoadAsset for Sound {
         export function setup_audio(audio, handler) {
             audio.oncanplaythrough = function() { handler(true); };
             audio.onerror = function() { handler(false); };
+            audio.load();
         }
         "#)]
         extern "C" {
