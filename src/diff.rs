65be10e83d333d5ea8fe52dc065d4e5cf8b0507d
kuviman <kuviman@gmail.com>
2021-09-26 15:37:27 +0400

    Diff: Unpin

--- src/diff.rs
+++ src/diff.rs
@@ -1,15 +1,30 @@
 use super::*;
 
 pub trait Diff:
-    Debug + Serialize + for<'de> Deserialize<'de> + Sync + Send + Clone + PartialEq + 'static
+    Debug + Serialize + for<'de> Deserialize<'de> + Sync + Send + Clone + PartialEq + 'static + Unpin
 {
-    type Delta: Debug + Serialize + for<'de> Deserialize<'de> + Sync + Send + Clone + 'static;
+    type Delta: Debug
+        + Serialize
+        + for<'de> Deserialize<'de>
+        + Sync
+        + Send
+        + Clone
+        + 'static
+        + Unpin;
     fn diff(&self, to: &Self) -> Self::Delta;
     fn update(&mut self, delta: &Self::Delta);
 }
 
 impl<
-        T: Debug + Serialize + for<'de> Deserialize<'de> + Sync + Send + Copy + PartialEq + 'static,
+        T: Debug
+            + Serialize
+            + for<'de> Deserialize<'de>
+            + Sync
+            + Send
+            + Copy
+            + PartialEq
+            + 'static
+            + Unpin,
     > Diff for T
 {
     type Delta = Self;
