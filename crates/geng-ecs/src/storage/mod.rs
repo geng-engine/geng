f459981f4dc7bc5b1f199046739d5e5793715514
kuviman <kuviman@gmail.com>
2021-10-14 07:38:09 +0400

    GAT waiting room

--- crates/geng-ecs/src/storage/mod.rs
+++ crates/geng-ecs/src/storage/mod.rs
@@ -5,3 +5,24 @@ pub mod world;
 
 pub use entity::Storage as Entity;
 pub use world::Storage as World;
+
+// GAT waiting room
+// https://github.com/rust-lang/rust/issues/44265
+
+// pub unsafe trait Storage {
+//     type Ref<'a, T: Component>: Ref<'a, T>;
+//     fn borrow<'a, T: Component>(&'a self) -> Self::Ref<'a, T>;
+
+//     type RefMut<'a, T: Component>: Ref<'a, T>;
+//     fn borrow_mut<'a, T: Component>(&'a self) -> Self::RefMut<'a, T>;
+// }
+
+// pub unsafe trait Ref<'a, T: Component> {
+//     type Key;
+//     fn get(&self, key: Self::Key) -> &'a T;
+// }
+
+// pub unsafe trait RefMut<'a, T: Component> {
+//     type Key;
+//     fn get(&self, key: Self::Key) -> &'a mut T;
+// }
