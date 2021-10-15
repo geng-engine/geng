ea8d93070199c1529d547756fe6e36a8489370dc
kuviman <kuviman@gmail.com>
2021-10-15 18:45:36 +0400

    switch to env_logger

--- crates/batbox/src/logger.rs
+++ crates/batbox/src/logger.rs
@@ -1,24 +1,30 @@
 use super::*;
 
-struct Logger;
+struct Logger {
+    inner: env_logger::Logger,
+}
 
 static LOGGERS: once_cell::sync::Lazy<Mutex<Vec<Box<dyn log::Log>>>> =
     once_cell::sync::Lazy::new(|| Mutex::new(Vec::new()));
 
 impl log::Log for Logger {
     fn enabled(&self, metadata: &log::Metadata) -> bool {
-        if metadata.level() > log::max_level() {
-            return false;
-        }
-        match metadata.target().split_terminator(':').next().unwrap() {
-            "ws" => metadata.level() <= log::Level::Error,
-            "mio" => false,
-            _ => true,
-        }
+        self.inner.enabled(metadata)
     }
+    // fn foo() {
+    //     if metadata.level() > log::max_level() {
+    //         return false;
+    //     }
+    //     match metadata.target().split_terminator(':').next().unwrap() {
+    //         "ws" => metadata.level() <= log::Level::Error,
+    //         "mio" => false,
+    //         _ => true,
+    //     }
+    // }
 
     fn log(&self, record: &log::Record) {
-        if self.enabled(record.metadata()) {
+        self.inner.log(record);
+        if self.inner.matches(record) {
             for logger in LOGGERS.lock().unwrap().iter_mut() {
                 if logger.enabled(record.metadata()) {
                     logger.log(record);
@@ -36,33 +42,46 @@ impl log::Log for Logger {
         }
     }
 
-    fn flush(&self) {}
+    fn flush(&self) {
+        self.inner.flush();
+        for logger in LOGGERS.lock().unwrap().iter_mut() {
+            logger.flush();
+        }
+    }
 }
 
-static LOGGER: Logger = Logger;
-
-pub fn init_with_level(level: log::LevelFilter) -> Result<(), log::SetLoggerError> {
-    log::set_logger(&LOGGER)?;
+pub fn init_with(mut builder: env_logger::Builder) -> Result<(), log::SetLoggerError> {
+    let builder_info = format!("{:?}", builder);
+    let logger = Logger {
+        inner: builder.build(),
+    };
+    log::set_max_level(logger.inner.filter());
+    log::set_boxed_logger(Box::new(logger))?;
+    trace!("Logger initialized with {}", builder_info);
     std::panic::set_hook(Box::new(|info| {
         error!("{}", info);
         error!("{:?}", backtrace::Backtrace::new());
     }));
-    log::set_max_level(level);
-    #[cfg(not(target_arch = "wasm32"))]
-    add_logger(Box::new({
-        let mut logger = stderrlog::new();
-        logger.verbosity(4);
-        logger
-    }));
     Ok(())
 }
 
+pub fn builder() -> env_logger::Builder {
+    let mut builder = env_logger::Builder::new();
+    builder
+        .filter_level(if cfg!(debug_assertions) {
+            log::LevelFilter::Debug
+        } else {
+            log::LevelFilter::Info
+        })
+        .format_timestamp(None)
+        .format_module_path(false)
+        .format_target(false)
+        .parse_env("LOG");
+    builder
+}
+
 pub fn init() -> Result<(), log::SetLoggerError> {
-    if cfg!(debug_assertions) {
-        init_with_level(log::LevelFilter::Debug)
-    } else {
-        init_with_level(log::LevelFilter::Info)
-    }
+    init_with(builder())
 }
 
 pub fn add_logger(logger: Box<dyn log::Log>) {
