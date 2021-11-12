fdfc18052185d2284d76dcfd28eac3163633e9a6
kuviman <kuviman@gmail.com>
2021-11-13 01:16:40 +0400

    Fix ellipse antialasing

--- crates/geng/src/draw_2d/ellipse.glsl
+++ crates/geng/src/draw_2d/ellipse.glsl
@@ -1,15 +1,14 @@
 uniform mat3 u_projection_matrix;
 uniform mat3 u_view_matrix;
+uniform mat3 u_model_matrix;
 varying vec2 v_quad_pos;
 uniform ivec2 u_framebuffer_size;
 
 #ifdef VERTEX_SHADER
 attribute vec2 a_quad_pos;
-uniform vec2 u_pos;
-uniform vec2 u_radius;
 void main() {
     v_quad_pos = a_quad_pos;
-    vec3 pos = u_projection_matrix * u_view_matrix * vec3(u_pos + u_radius * a_quad_pos, 1.0);
+    vec3 pos = u_projection_matrix * u_view_matrix * u_model_matrix * vec3(a_quad_pos, 1.0);
     gl_Position = vec4(pos.xy, 0.0, pos.z);
 }
 #endif
@@ -22,9 +21,13 @@ void main() {
     }
     gl_FragColor = u_color;
 #ifdef GENG_ANTIALIAS
-    vec3 pixelV = inverse(u_projection_matrix * u_view_matrix) * vec3(vec2(1.0, 1.0) / vec2(u_framebuffer_size), 0.0);
-    float pixelR = length(pixelV.xy);
-    gl_FragColor.w *= min((1.0 - length(v_quad_pos)) / (pixelR / 1.0), 1.0);
+    mat3 im = inverse(u_projection_matrix * u_view_matrix * u_model_matrix);
+    vec3 pixelP1 = im * vec3(vec2(0.0, 0.0) * 2.0 / vec2(u_framebuffer_size), 1.0);
+    pixelP1.xy /= pixelP1.z;
+    vec3 pixelP2 = im * vec3(vec2(1.0, 1.0) * 2.0 / vec2(u_framebuffer_size), 1.0);
+    pixelP2.xy /= pixelP2.z;
+    float pixelLength = length((pixelP2 - pixelP1).xy);
+    gl_FragColor.w *= min((1.0 - length(v_quad_pos)) / pixelLength, 1.0);
 #endif
 }
 #endif
