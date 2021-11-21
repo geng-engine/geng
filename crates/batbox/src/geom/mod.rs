90f9f1a59b485fa736d9afcbfb29e72978d0da79
Nertsal <sasha.kudasov04@gmail.com>
2021-11-21 22:06:05 +0300

    Start making Chain & Segment

--- crates/batbox/src/geom/mod.rs
+++ crates/batbox/src/geom/mod.rs
@@ -1,13 +1,17 @@
 use super::*;
 
 mod aabb;
+mod chain;
 mod mat;
 mod quad;
 mod quat;
+mod segment;
 mod vec;
 
 pub use aabb::*;
+pub use chain::*;
 pub use mat::*;
 pub use quad::*;
 pub use quat::*;
+pub use segment::*;
 pub use vec::*;
