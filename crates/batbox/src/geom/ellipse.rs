820dd11dcc6965620d971f71a35a10a598aee9d6
kuviman <kuviman@gmail.com>
2021-11-21 19:58:16 +0400

    impl FitTarget2d for Ellipse

--- crates/batbox/src/geom/ellipse.rs
+++ crates/batbox/src/geom/ellipse.rs
@@ -29,3 +29,15 @@ impl Transform2d for Ellipse {
         self.matrix = transform * self.matrix;
     }
 }
+
+impl FitTarget2d for Ellipse {
+    fn make_fit(&self, object: &mut impl Transform2d) {
+        let inversed_matrix = self.matrix().inverse();
+        let quad_in_circle = object.bounding_quad().transform(inversed_matrix);
+        let center = (quad_in_circle.matrix() * vec3(0.0, 0.0, 1.0)).into_2d();
+        let corner = (quad_in_circle.matrix() * vec3(1.0, 1.0, 1.0)).into_2d();
+        let local_transform =
+            Mat3::scale_uniform(1.0 / (corner - center).len()) * Mat3::translate(-center);
+        object.apply_transform(self.matrix() * local_transform * inversed_matrix)
+    }
+}
