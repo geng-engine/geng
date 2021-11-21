70db805e65e9690539ef10992e8a46a9b91e2034
kuviman <kuviman@gmail.com>
2021-11-21 11:03:42 +0400

    Rendered texture in examples::draw

--- crates/geng/examples/draw.rs
+++ crates/geng/examples/draw.rs
@@ -3,15 +3,51 @@ use geng::prelude::*;
 struct State {
     geng: Geng,
     texture: ugli::Texture,
+    rendered_texture: ugli::Texture,
 }
 
 impl State {
     fn new(geng: &Geng) -> Self {
         Self {
             geng: geng.clone(),
-            texture: ugli::Texture::new_with(geng.ugli(), vec2(2, 2), |pos| {
-                Color::rgb(pos.x as f32, pos.y as f32, 0.0)
+            texture: ugli::Texture::new_with(geng.ugli(), vec2(2, 2), |pos| match (pos.x, pos.y) {
+                (0, 0) => Color::BLACK,
+                (1, 0) => Color::RED,
+                (1, 1) => Color::GREEN,
+                (0, 1) => Color::BLUE,
+                _ => unreachable!(),
             }),
+            rendered_texture: {
+                const SIZE: usize = 128;
+                let mut texture = ugli::Texture::new_uninitialized(geng.ugli(), vec2(SIZE, SIZE));
+                let mut framebuffer = ugli::Framebuffer::new_color(
+                    geng.ugli(),
+                    ugli::ColorAttachment::Texture(&mut texture),
+                );
+                geng.draw_2d(
+                    &mut framebuffer,
+                    &geng::PixelPerfectCamera,
+                    &draw_2d::Polygon::new_gradient(vec![
+                        draw_2d::ColoredVertex {
+                            a_pos: vec2(0.0, 0.0),
+                            a_color: Color::BLACK,
+                        },
+                        draw_2d::ColoredVertex {
+                            a_pos: vec2(SIZE as f32, 0.0),
+                            a_color: Color::RED,
+                        },
+                        draw_2d::ColoredVertex {
+                            a_pos: vec2(SIZE as f32, SIZE as f32),
+                            a_color: Color::GREEN,
+                        },
+                        draw_2d::ColoredVertex {
+                            a_pos: vec2(0.0, SIZE as f32),
+                            a_color: Color::BLUE,
+                        },
+                    ]),
+                );
+                texture
+            },
         }
     }
 }
@@ -30,6 +66,9 @@ impl geng::State for State {
                 .transform(Mat3::rotate(0.5) * Mat3::scale_uniform(0.5)),
         ));
         objects.push(Box::new(draw_2d::TexturedQuad::unit(&self.texture)));
+        objects.push(Box::new(draw_2d::TexturedQuad::unit(
+            &self.rendered_texture,
+        )));
         objects.push(Box::new(draw_2d::Ellipse::unit(Color::RED)));
         objects.push(Box::new(
             draw_2d::Ellipse::unit_with_cut(0.5, Color::RED)
