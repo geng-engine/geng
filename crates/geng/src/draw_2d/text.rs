7b739ad9678a9398ea4ea37a480d9bdb3cfac846
kuviman <kuviman@gmail.com>
2022-02-02 14:08:05 +0400

    draw_2d::Text::true_transform

--- crates/geng/src/draw_2d/text.rs
+++ crates/geng/src/draw_2d/text.rs
@@ -1,11 +1,12 @@
 use super::*;
 
 pub struct Text<F: std::borrow::Borrow<Font>, T: AsRef<str>> {
-    font: F,
-    text: T,
-    color: Color<f32>,
-    into_unit_transform: Mat3<f32>,
-    transform: Mat3<f32>,
+    pub font: F,
+    pub text: T,
+    pub color: Color<f32>,
+    pub into_unit_transform: Mat3<f32>,
+    pub transform: Mat3<f32>,
+    pub true_transform: Mat3<f32>, // TODO: only have this
 }
 
 const SIZE_HACK: f32 = 1000.0;
@@ -25,6 +26,7 @@ impl<F: std::borrow::Borrow<Font>, T: AsRef<str>> Text<F, T> {
                     * Mat3::scale(aabb.size() / 2.0))
                 .inverse(),
                 transform: Mat3::scale(vec2(aspect, 1.0)),
+                true_transform: Mat3::identity(),
             }
         } else {
             Self {
@@ -33,6 +35,7 @@ impl<F: std::borrow::Borrow<Font>, T: AsRef<str>> Text<F, T> {
                 color,
                 into_unit_transform: Mat3::identity(),
                 transform: Mat3::scale_uniform(0.0),
+                true_transform: Mat3::identity(),
             }
         }
     }
@@ -46,6 +49,7 @@ impl<F: std::borrow::Borrow<Font>, T: AsRef<str>> Transform2d<f32> for Text<F, T
     }
     fn apply_transform(&mut self, transform: Mat3<f32>) {
         self.transform = transform * self.transform;
+        self.true_transform = transform * self.true_transform;
     }
 }
 
