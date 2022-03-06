0c12f5cade567274b44d232ed2a3dbedb3be4dd2
kuviman <kuviman@gmail.com>
2022-03-06 12:11:37 +0400

    Deriving ugli::Vertex with darling

--- crates/ugli-derive/src/lib.rs
+++ crates/ugli-derive/src/lib.rs
@@ -25,29 +25,9 @@ mod vertex;
 
 #[proc_macro_derive(Vertex)]
 pub fn derive_vertex(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    vertex::derive(input.into()).into()
-}
-
-fn simple_derive(
-    input: TokenStream,
-    safe: bool,
-    typ: syn::Path,
-    expand: fn(&syn::DeriveInput) -> TokenStream,
-) -> TokenStream {
-    let s = input.to_string();
-    let ast: syn::DeriveInput = syn::parse_str(&s).unwrap();
-    let input_type = &ast.ident;
-    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
-    let impl_body = expand(&ast);
-    let safe = if safe {
-        quote! {}
-    } else {
-        quote! { unsafe }
-    };
-    quote! {
-        #safe
-        impl #impl_generics #typ for #input_type #ty_generics #where_clause {
-            #impl_body
-        }
+    let input: syn::DeriveInput = syn::parse_macro_input!(input);
+    match vertex::DeriveInput::from_derive_input(&input) {
+        Ok(input) => input.derive().into(),
+        Err(e) => e.write_errors().into(),
     }
 }
--- crates/ugli-derive/src/vertex.rs
+++ crates/ugli-derive/src/vertex.rs
@@ -1,24 +1,43 @@
 use super::*;
 
-pub fn derive(input: TokenStream) -> TokenStream {
-    simple_derive(
-        input,
-        false,
-        syn::parse_str("ugli::Vertex").unwrap(),
-        expand,
-    )
+#[derive(FromDeriveInput)]
+#[darling(supports(struct_any))]
+pub struct DeriveInput {
+    ident: syn::Ident,
+    generics: syn::Generics,
+    data: darling::ast::Data<(), Field>,
 }
 
-pub fn expand(input: &syn::DeriveInput) -> TokenStream {
-    match input.data {
-        syn::Data::Struct(syn::DataStruct { ref fields, .. }) => {
-            let field_names = fields.iter().map(|field| field.ident.as_ref().unwrap());
-            let field_names_copy = fields.iter().map(|field| field.ident.as_ref().unwrap());
-            quote! {
+#[derive(FromField)]
+struct Field {
+    ident: Option<syn::Ident>,
+}
+
+impl DeriveInput {
+    pub fn derive(self) -> TokenStream {
+        let Self {
+            ident,
+            generics,
+            data,
+        } = self;
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+        let data = data.take_struct().unwrap();
+        let field_names = data.fields.iter().enumerate().map(|(index, field)| {
+            field
+                .ident
+                .as_ref()
+                .map(|ident| quote! { #ident })
+                .unwrap_or_else(|| {
+                    let index = syn::Index::from(index);
+                    quote! { #index }
+                })
+        });
+        quote! {
+            unsafe impl #impl_generics ugli::Vertex for #ident #ty_generics #where_clause {
                 unsafe fn walk_attributes<C>(sample: *const Self, mut visitor: C)
                     where C: ugli::VertexAttributeVisitor {
                     #(visitor.visit(
-                        stringify!(#field_names_copy),
+                        stringify!(#field_names),
                         unsafe {
                             std::ptr::addr_of!((*sample).#field_names)
                         },
@@ -26,6 +45,5 @@ pub fn expand(input: &syn::DeriveInput) -> TokenStream {
                 }
             }
         }
-        _ => panic!("ugli::Vertex can only be derived by structs"),
     }
 }
