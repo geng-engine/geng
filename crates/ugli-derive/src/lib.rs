372aad7c8403565d89484a026bc85dfe16532b0e
kuviman <kuviman@gmail.com>
2022-03-06 12:09:22 +0400

    deriving ugli::Uniforms with darling

--- crates/ugli-derive/src/lib.rs
+++ crates/ugli-derive/src/lib.rs
@@ -7,13 +7,18 @@ extern crate proc_macro;
 extern crate quote;
 
 use batbox::*;
+use darling::{FromDeriveInput, FromField, FromMeta};
 use proc_macro2::TokenStream;
 
 mod uniforms;
 
 #[proc_macro_derive(Uniforms)]
 pub fn derive_uniforms(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    uniforms::derive(input.into()).into()
+    let input: syn::DeriveInput = syn::parse_macro_input!(input);
+    match uniforms::DeriveInput::from_derive_input(&input) {
+        Ok(input) => input.derive().into(),
+        Err(e) => e.write_errors().into(),
+    }
 }
 
 mod vertex;
--- crates/ugli-derive/src/uniforms.rs
+++ crates/ugli-derive/src/uniforms.rs
@@ -1,25 +1,43 @@
 use super::*;
 
-pub fn derive(input: TokenStream) -> TokenStream {
-    simple_derive(
-        input,
-        true,
-        syn::parse_str("ugli::Uniforms").unwrap(),
-        expand,
-    )
+#[derive(FromDeriveInput)]
+#[darling(supports(struct_any))]
+pub struct DeriveInput {
+    ident: syn::Ident,
+    generics: syn::Generics,
+    data: darling::ast::Data<(), Field>,
 }
 
-fn expand(input: &syn::DeriveInput) -> TokenStream {
-    match input.data {
-        syn::Data::Struct(syn::DataStruct { ref fields, .. }) => {
-            let field_names = fields.iter().map(|field| field.ident.as_ref().unwrap());
-            let field_names_copy = fields.iter().map(|field| field.ident.as_ref().unwrap());
-            quote! {
+#[derive(FromField)]
+struct Field {
+    ident: Option<syn::Ident>,
+}
+
+impl DeriveInput {
+    pub fn derive(self) -> TokenStream {
+        let Self {
+            ident,
+            generics,
+            data,
+        } = self;
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+        let data = data.take_struct().unwrap();
+        let field_names = data.fields.iter().enumerate().map(|(index, field)| {
+            field
+                .ident
+                .as_ref()
+                .map(|ident| quote! { #ident })
+                .unwrap_or_else(|| {
+                    let index = syn::Index::from(index);
+                    quote! { #index }
+                })
+        });
+        quote! {
+            impl #impl_generics ugli::Uniforms for #ident #ty_generics #where_clause {
                 fn walk_uniforms<C>(&self, visitor: &mut C) where C: ugli::UniformVisitor {
-                    #(visitor.visit(stringify!(#field_names_copy), &self.#field_names));*
+                    #(visitor.visit(stringify!(#field_names), &self.#field_names));*
                 }
             }
         }
-        _ => panic!("ugli::Uniforms can only be derived by structs"),
     }
 }
