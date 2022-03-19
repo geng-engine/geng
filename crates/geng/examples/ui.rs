667bc902d1d1570d8c9b50711ba4af5fa0c4ba32
kuviman <kuviman@gmail.com>
2022-03-19 12:02:49 +0400

    ui counter example

--- crates/geng/examples/ui.rs
+++ crates/geng/examples/ui.rs
@@ -2,11 +2,19 @@ use geng::prelude::*;
 
 struct State {
     geng: Geng,
+    counter: i32,
+    minus_button: geng::ui::Button,
+    plus_button: geng::ui::Button,
 }
 
 impl State {
     fn new(geng: &Geng) -> Self {
-        Self { geng: geng.clone() }
+        Self {
+            geng: geng.clone(),
+            counter: 0,
+            minus_button: geng::ui::Button::new(),
+            plus_button: geng::ui::Button::new(),
+        }
     }
 }
 
@@ -17,7 +25,36 @@ impl geng::State for State {
     fn ui(&mut self) -> Box<dyn geng::ui::Widget + '_> {
         use geng::ui;
         use geng::ui::*;
-        Box::new(ui::Text::new("Hello, UI!", self.geng.default_font(), 32.0, Color::WHITE).center())
+        let theme = Rc::new(ui::Theme::dark(&self.geng));
+        if self.minus_button.clicked() {
+            self.counter -= 1;
+        }
+        if self.plus_button.clicked() {
+            self.counter += 1;
+        }
+        Box::new(
+            ui::column![
+                ui::Text::new(
+                    "Counter example",
+                    self.geng.default_font(),
+                    32.0,
+                    Color::WHITE
+                )
+                .center(),
+                row![
+                    ui::Button::text(&mut self.minus_button, "-", &theme).padding_right(32.0),
+                    ui::Text::new(
+                        self.counter.to_string(),
+                        self.geng.default_font(),
+                        32.0,
+                        Color::WHITE
+                    ),
+                    ui::Button::text(&mut self.plus_button, "+", &theme).padding_left(32.0),
+                ]
+                .center(),
+            ]
+            .center(),
+        )
     }
 }
 
