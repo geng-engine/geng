f0c9d975a921a97c5807a75b0caa85f76ce943fb
kuviman <kuviman@gmail.com>
2022-03-29 07:09:37 +0400

    ui::Slider

--- crates/geng/src/ui/widgets/slider.rs
+++ crates/geng/src/ui/widgets/slider.rs
@@ -1,37 +1,27 @@
 use super::*;
 
-pub struct Slider {
-    theme: Rc<Theme>,
-    tick_radius: f32,
+pub struct Slider<'a> {
+    sense: &'a mut Sense,
+    pos: &'a mut Option<AABB<f64>>,
+    tick_radius: &'a mut f32,
+    value: f64,
+    range: RangeInclusive<f64>,
+    f: Box<dyn FnMut(f64) + 'a>,
 }
 
-impl Clone for Slider {
-    fn clone(&self) -> Self {
-        Self {
-            theme: self.theme.clone(),
-            tick_radius: 0.0,
-        }
-    }
-}
+impl<'a> Slider<'a> {
+    const ANIMATION_SPEED: f32 = 5.0;
 
-impl Slider {
-    pub fn new(theme: &Rc<Theme>) -> Self {
-        Self {
-            theme: theme.clone(),
-            core: WidgetCore::new(),
-            tick_radius: 0.0,
-        }
-    }
-    pub fn ui<'a>(
-        &'a mut self,
+    pub fn new(
+        cx: &'a Controller,
         value: f64,
         range: RangeInclusive<f64>,
         f: Box<dyn FnMut(f64) + 'a>,
-    ) -> impl Widget + 'a {
-        SliderUI {
-            theme: &self.theme,
-            tick_radius: &mut self.tick_radius,
-            core: &mut self.core,
+    ) -> Self {
+        Slider {
+            sense: cx.get_state(),
+            tick_radius: cx.get_state(),
+            pos: cx.get_state(),
             value,
             range,
             f,
@@ -39,47 +29,31 @@ impl Slider {
     }
 }
 
-pub struct SliderUI<'a> {
-    theme: &'a Theme,
-    core: &'a mut WidgetCore,
-    tick_radius: &'a mut f32,
-    value: f64,
-    range: RangeInclusive<f64>,
-    f: Box<dyn FnMut(f64) + 'a>,
-}
-
-impl SliderUI<'_> {
-    const ANIMATION_SPEED: f32 = 5.0;
-}
-
-impl<'a> Widget for SliderUI<'a> {
-    fn core(&self) -> &WidgetCore {
-        self.core
-    }
-    fn core_mut(&mut self) -> &mut WidgetCore {
-        self.core
+impl<'a> Widget for Slider<'a> {
+    fn sense(&mut self) -> Option<&mut Sense> {
+        Some(self.sense)
     }
     fn update(&mut self, delta_time: f64) {
-        let height = self.core.position().height() as f32;
-        let target_tick_radius = if self.core.hovered() || self.core.captured() {
-            height / 2.0
+        let target_tick_radius = if self.sense.is_hovered() || self.sense.is_captured() {
+            1.0 / 2.0
         } else {
-            height / 6.0
+            1.0 / 6.0
         };
         *self.tick_radius += (target_tick_radius - *self.tick_radius)
-            .clamp_abs(Self::ANIMATION_SPEED * delta_time as f32 * height);
+            .clamp_abs(Self::ANIMATION_SPEED * delta_time as f32);
     }
-    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
-        let geng = self.theme.geng();
+    fn draw(&mut self, cx: &mut DrawContext) {
+        *self.pos = Some(cx.position);
+        let geng = cx.geng;
         let draw_2d = geng.draw_2d_helper();
-        let position = self.core.position().map(|x| x as f32);
+        let position = cx.position.map(|x| x as f32);
         let line_width = position.height() / 3.0;
         let value_position = *self.tick_radius
             + ((self.value - *self.range.start()) / (*self.range.end() - *self.range.start()))
                 as f32
                 * (position.width() - *self.tick_radius * 2.0);
         geng.draw_2d(
-            framebuffer,
+            cx.framebuffer,
             &PixelPerfectCamera,
             &draw_2d::Quad::new(
                 AABB::from_corners(
@@ -88,11 +62,11 @@ impl<'a> Widget for SliderUI<'a> {
                     position.bottom_left()
                         + vec2(value_position, (position.height() + line_width) / 2.0),
                 ),
-                self.theme.hover_color,
+                cx.theme.hover_color,
             ),
         );
         geng.draw_2d(
-            framebuffer,
+            cx.framebuffer,
             &PixelPerfectCamera,
             &draw_2d::Quad::new(
                 AABB::from_corners(
@@ -101,44 +75,51 @@ impl<'a> Widget for SliderUI<'a> {
                     position.top_right()
                         - vec2(line_width / 2.0, (position.height() - line_width) / 2.0),
                 ),
-                self.theme.usable_color,
+                cx.theme.usable_color,
             ),
         );
         geng.draw_2d(
-            framebuffer,
+            cx.framebuffer,
             &PixelPerfectCamera,
             &draw_2d::Ellipse::circle(
                 position.bottom_left() + vec2(line_width / 2.0, position.height() / 2.0),
                 line_width / 2.0,
-                self.theme.hover_color,
+                cx.theme.hover_color,
             ),
         );
         draw_2d.circle(
-            framebuffer,
+            cx.framebuffer,
             &PixelPerfectCamera,
             position.top_right() - vec2(line_width / 2.0, position.height() / 2.0),
             line_width / 2.0,
-            self.theme.usable_color,
+            cx.theme.usable_color,
         );
         draw_2d.circle(
-            framebuffer,
+            cx.framebuffer,
             &PixelPerfectCamera,
             position.bottom_left() + vec2(value_position, position.height() / 2.0),
-            *self.tick_radius,
-            self.theme.hover_color,
+            *self.tick_radius * position.height(),
+            cx.theme.hover_color,
         );
     }
     fn handle_event(&mut self, event: &Event) {
-        if self.core.captured() {
-            if let Event::MouseDown { position, .. } | Event::MouseMove { position } = &event {
-                let position = position.x - self.core.position().x_min;
+        let aabb = match *self.pos {
+            Some(pos) => pos,
+            None => return,
+        };
+        if self.sense.is_captured() {
+            if let Event::MouseDown { position, .. } | Event::MouseMove { position, .. } = &event {
+                let position = position.x - aabb.x_min;
                 let new_value = *self.range.start()
-                    + ((position - self.core.position().height() / 2.0)
-                        / (self.core.position().width() - self.core.position().height()))
-                    .clamp(0.0, 1.0)
+                    + ((position - aabb.height() / 2.0) / (aabb.width() - aabb.height()))
+                        .clamp(0.0, 1.0)
                         * (*self.range.end() - *self.range.start());
                 (self.f)(new_value);
             }
         }
+    }
+
+    fn calc_constraints(&mut self, children: &ConstraintsContext) -> Constraints {
+        Constraints::default()
     }
 }
