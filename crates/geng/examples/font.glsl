8c08b3c6d6e8b4ae21a4df5e6fe80ffb6e23ad71
kuviman <kuviman@gmail.com>
2022-08-06 14:19:05 +0400

    Simplify font.glsl

--- crates/geng/examples/font.glsl
+++ crates/geng/examples/font.glsl
@@ -19,29 +19,17 @@ void main() {
 #ifdef FRAGMENT_SHADER
 uniform sampler2D u_texture;
 
-float invOrZero(float x) {
-    return abs(sign(x)) / (x + (1.0 - abs(sign(x))));
-}
-
-float invOrOne(float x) {
-    return 1.0 / (x + (1.0 - abs(sign(x))));
-}
-
-float stepSmooth(float a, float b, float x) {
-    float t = clamp((x - a) / (b - a), 0.0, 1.0);
-    return t * t * (3.0 - 2.0 * t);
-}
-
-float aa(float edge, float x, float w) {
-    return stepSmooth(edge - w, edge + w, x);
+float aa(float edge, float x) {
+    float w = length(vec2(dFdx(x), dFdy(x)));
+    return smoothstep(edge - w, edge + w, x);
 }
 
 void main() {
     vec4 sample = texture2D(u_texture, v_uv);
     float dist = (sample.x - 0.5) * 2.0;
     float w = length(vec2(dFdx(dist), dFdy(dist)));
-    float inside = aa(0.0, dist, w);
-    float inside_border = aa(-0.15, dist, w);
+    float inside = aa(0.0, dist);
+    float inside_border = aa(-0.15, dist);
     vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
     vec4 border_color = vec4(1.0, 1.0, 1.0, 1.0);
     gl_FragColor = color * inside + (1.0 - inside) * (border_color * inside_border + vec4(border_color.xyz, 0.0) * (1.0 - inside_border));
