7ae25e8c8461a230d3ce5057c29fef6e28c749d3
kuviman <kuviman@gmail.com>
2023-01-24 04:11:23 +0400

    Document num

--- crates/batbox/src/num/real.rs
+++ crates/batbox/src/num/real.rs
@@ -1,32 +1,187 @@
 use super::*;
 
+/// Real number (differs from [Float] as doesn't support NaN)
 pub trait Real: Num + Copy {
+    /// Archimedes’ constant (π)
     const PI: Self;
-    fn signum(self) -> Self;
-    fn floor(self) -> Self;
-    fn ceil(self) -> Self;
-    fn sqrt(self) -> Self;
-    fn tan(self) -> Self;
-    fn sin(self) -> Self;
-    fn cos(self) -> Self;
-    fn sin_cos(self) -> (Self, Self) {
-        (self.sin(), self.cos())
-    }
+
+    /// Computes the arccosine of a number.
+    ///
+    /// Return value is in radians in the range [0, pi] or NaN if the number is outside the range [-1, 1].
+    fn acos(self) -> Self;
+
+    /// Computes the arcsine of a number.
+    ///
+    /// Return value is in radians in the range [-pi/2, pi/2] or NaN if the number is outside the range [-1, 1].
+    fn asin(self) -> Self;
+
+    /// Computes the arctangent of a number.
+    ///
+    /// Return value is in radians in the range [-pi/2, pi/2];
+    fn atan(self) -> Self;
+
+    /// Computes the four quadrant arctangent of `self` (`y`) and `other` (`x`) in radians.  
+    ///
+    /// * `x = 0`, `y = 0`: `0`
+    /// * `x >= 0`: `arctan(y/x)` -> `[-pi/2, pi/2]`
+    /// * `y >= 0`: `arctan(y/x) + pi` -> `(pi/2, pi]`
+    /// * `y < 0`: `arctan(y/x) - pi` -> `(-pi, -pi/2)`
     fn atan2(y: Self, x: Self) -> Self;
+
+    /// Returns the smallest integer greater than or equal to a number.
+    fn ceil(self) -> Self;
+
+    /// Computes the cosine of a number (in radians).
+    fn cos(self) -> Self;
+
+    /// Calculates Euclidean division, the matching method for rem_euclid.
+    ///
+    /// This computes the integer n such that self = n * rhs + self.rem_euclid(rhs).
+    /// In other words, the result is self / rhs rounded to the integer n such that self >= n * rhs.
+    fn div_euclid(self, other: Self) -> Self;
+
+    /// Returns `e^(self)`, (the exponential function).
+    fn exp(self) -> Self;
+
+    /// Returns the largest integer less than or equal to `self`.
+    fn floor(self) -> Self;
+
+    /// Returns the fractional part of `self`.
+    fn fract(self) -> Self;
+
+    /// Returns the natural logarithm of the number.
+    fn ln(self) -> Self;
+
+    /// Returns the logarithm of the number with respect to an arbitrary base.
+    fn log(self, base: Self) -> Self;
+
+    /// Returns the base 10 logarithm of the number.
+    fn log10(self) -> Self;
+
+    /// Returns the base 2 logarithm of the number.
+    fn log2(self) -> Self;
+
+    /// Raises a number to a floating point power.
+    fn powf(self, n: Self) -> Self;
+
+    /// Raises a number to an integer power.
+    fn powi(self, n: i32) -> Self;
+
+    /// Takes the reciprocal (inverse) of a number, 1/x
+    fn recip(self) -> Self;
+
+    /// Calculates the least nonnegative remainder of `self (mod rhs)`.
+    ///
+    /// In particular, the return value `r` satisfies `0.0 <= r < rhs.abs()` in
+    /// most cases. However, due to a floating point round-off error it can
+    /// result in `r == rhs.abs()`, violating the mathematical definition, if
+    /// `self` is much smaller than `rhs.abs()` in magnitude and `self < 0.0`.
+    /// This result is not an element of the function's codomain, but it is the
+    /// closest floating point number in the real numbers and thus fulfills the
+    /// property `self == self.div_euclid(rhs) * rhs + self.rem_euclid(rhs)`
+    /// approximately.
+    fn rem_euclid(self, other: Self) -> Self;
+
+    /// Returns the nearest integer to `self`.
+    /// Round half-way cases away from `0.0`.
+    fn round(self) -> Self;
+
+    /// Returns a number that represents the sign of `self`.
+    ///
+    /// - `1.0` if the number is positive, `+0.0` or `INFINITY`
+    /// - `-1.0` if the number is negative, `-0.0` or `NEG_INFINITY`
+    /// - NaN if the number is NaN
+    fn signum(self) -> Self;
+
+    /// Computes the sine of a number (in radians).
+    fn sin(self) -> Self;
+
+    /// Simultaneously computes the sine and cosine of the number, `x`.
+    /// Returns `(sin(x), cos(x))`.
+    fn sin_cos(self) -> (Self, Self);
+
+    /// Returns the square root of a number.
+    ///
+    /// Returns NaN if `self` is a negative number other than `-0.0`.
+    fn sqrt(self) -> Self;
+
+    /// Computes the tangent of a number (in radians).
+    fn tan(self) -> Self;
+
+    /// Convert an [f32] into [Self]
     fn from_f32(x: f32) -> Self;
+
+    /// Convert self into an [f32]
     fn as_f32(self) -> f32;
 }
 
 impl<T: Real> Float for T {
     const PI: Self = <Self as Real>::PI;
-    fn signum(self) -> Self {
-        Real::signum(self)
+    fn acos(self) -> Self {
+        Real::acos(self)
+    }
+    fn asin(self) -> Self {
+        Real::asin(self)
+    }
+    fn atan(self) -> Self {
+        Real::atan(self)
+    }
+    fn atan2(y: Self, x: Self) -> Self {
+        Real::atan2(y, x)
+    }
+    fn ceil(self) -> Self {
+        Real::ceil(self)
+    }
+    fn cos(self) -> Self {
+        Real::cos(self)
+    }
+    fn div_euclid(self, other: Self) -> Self {
+        Real::div_euclid(self, other)
+    }
+    fn exp(self) -> Self {
+        Real::exp(self)
     }
     fn floor(self) -> Self {
         Real::floor(self)
     }
-    fn ceil(self) -> Self {
-        Real::ceil(self)
+    fn fract(self) -> Self {
+        Real::fract(self)
+    }
+    fn ln(self) -> Self {
+        Real::ln(self)
+    }
+    fn log(self, base: Self) -> Self {
+        Real::log(self, base)
+    }
+    fn log10(self) -> Self {
+        Real::log10(self)
+    }
+    fn log2(self) -> Self {
+        Real::log2(self)
+    }
+    fn powf(self, n: Self) -> Self {
+        Real::powf(self, n)
+    }
+    fn powi(self, n: i32) -> Self {
+        Real::powi(self, n)
+    }
+    fn recip(self) -> Self {
+        Real::recip(self)
+    }
+    fn rem_euclid(self, other: Self) -> Self {
+        Real::rem_euclid(self, other)
+    }
+    fn round(self) -> Self {
+        Real::round(self)
+    }
+    fn signum(self) -> Self {
+        Real::signum(self)
+    }
+    fn sin(self) -> Self {
+        Real::sin(self)
+    }
+    fn sin_cos(self) -> (Self, Self) {
+        Real::sin_cos(self)
     }
     fn sqrt(self) -> Self {
         Real::sqrt(self)
@@ -34,29 +189,18 @@ impl<T: Real> Float for T {
     fn tan(self) -> Self {
         Real::tan(self)
     }
-    fn sin(self) -> Self {
-        Real::sin(self)
-    }
-    fn cos(self) -> Self {
-        Real::cos(self)
-    }
-    fn sin_cos(self) -> (Self, Self) {
-        Real::sin_cos(self)
-    }
-    fn atan2(y: Self, x: Self) -> Self {
-        Real::atan2(y, x)
-    }
-    fn is_finite(self) -> bool {
-        true
-    }
     fn from_f32(x: f32) -> Self {
         Real::from_f32(x)
     }
     fn as_f32(self) -> f32 {
         Real::as_f32(self)
     }
+    fn is_finite(self) -> bool {
+        true
+    }
 }
 
+/// Wrapper around T that checks for valid values (panics on NaN/Inf)
 #[derive(Copy, Clone, PartialEq, Serialize)]
 #[repr(transparent)]
 pub struct RealImpl<T: Float>(T);
@@ -102,13 +246,18 @@ impl<T: Float> Display for RealImpl<T> {
 }
 
 impl<T: Float> RealImpl<T> {
+    /// Create a new value, panics if not finite
     pub fn new(value: T) -> Self {
         assert!(value.is_finite());
         Self(value)
     }
+
+    /// Create a new value without checking
     pub fn new_unchecked(value: T) -> Self {
         Self(value)
     }
+
+    /// Get raw value
     pub fn raw(self) -> T {
         self.0
     }
@@ -272,33 +421,78 @@ impl<T: Float + rand::distributions::uniform::SampleUniform>
 
 impl<T: Float> Real for RealImpl<T> {
     const PI: Self = Self(T::PI);
-    fn signum(self) -> Self {
-        Self::new(self.0.signum())
+    fn acos(self) -> Self {
+        Self::new(T::acos(self.0))
     }
-    fn floor(self) -> Self {
-        Self::new(self.0.floor())
+    fn asin(self) -> Self {
+        Self::new(T::asin(self.0))
     }
-    fn ceil(self) -> Self {
-        Self::new(self.0.ceil())
-    }
-    fn sqrt(self) -> Self {
-        Self::new(self.0.sqrt())
-    }
-    fn tan(self) -> Self {
-        Self::new(self.0.tan())
-    }
-    fn sin(self) -> Self {
-        Self::new(self.0.sin())
-    }
-    fn cos(self) -> Self {
-        Self::new(self.0.cos())
-    }
-    fn sin_cos(self) -> (Self, Self) {
-        let (sin, cos) = self.0.sin_cos();
-        (Self::new(sin), Self::new(cos))
+    fn atan(self) -> Self {
+        Self::new(T::atan(self.0))
     }
     fn atan2(y: Self, x: Self) -> Self {
         Self::new(T::atan2(y.0, x.0))
+    }
+    fn ceil(self) -> Self {
+        Self::new(T::ceil(self.0))
+    }
+    fn cos(self) -> Self {
+        Self::new(T::cos(self.0))
+    }
+    fn div_euclid(self, other: Self) -> Self {
+        Self::new(T::div_euclid(self.0, other.0))
+    }
+    fn exp(self) -> Self {
+        Self::new(T::exp(self.0))
+    }
+    fn floor(self) -> Self {
+        Self::new(T::floor(self.0))
+    }
+    fn fract(self) -> Self {
+        Self::new(T::fract(self.0))
+    }
+    fn ln(self) -> Self {
+        Self::new(T::ln(self.0))
+    }
+    fn log(self, base: Self) -> Self {
+        Self::new(T::log(self.0, base.0))
+    }
+    fn log10(self) -> Self {
+        Self::new(T::log10(self.0))
+    }
+    fn log2(self) -> Self {
+        Self::new(T::log2(self.0))
+    }
+    fn powf(self, n: Self) -> Self {
+        Self::new(T::powf(self.0, n.0))
+    }
+    fn powi(self, n: i32) -> Self {
+        Self::new(T::powi(self.0, n))
+    }
+    fn recip(self) -> Self {
+        Self::new(T::recip(self.0))
+    }
+    fn rem_euclid(self, other: Self) -> Self {
+        Self::new(T::rem_euclid(self.0, other.0))
+    }
+    fn round(self) -> Self {
+        Self::new(T::round(self.0))
+    }
+    fn signum(self) -> Self {
+        Self::new(T::signum(self.0))
+    }
+    fn sin(self) -> Self {
+        Self::new(T::sin(self.0))
+    }
+    fn sin_cos(self) -> (Self, Self) {
+        let (sin, cos) = T::sin_cos(self.0);
+        (Self::new(sin), Self::new(cos))
+    }
+    fn sqrt(self) -> Self {
+        Self::new(T::sqrt(self.0))
+    }
+    fn tan(self) -> Self {
+        Self::new(T::tan(self.0))
     }
     fn from_f32(x: f32) -> Self {
         Self::new(T::from_f32(x))
