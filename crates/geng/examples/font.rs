a18a2f2227a563dbf56258898a89db529fb50376
kuviman <kuviman@gmail.com>
2023-02-05 20:00:14 +0400

    font example show how to create sdf texture for given text

--- crates/geng/examples/font.rs
+++ crates/geng/examples/font.rs
@@ -1,6 +1,6 @@
 use geng::prelude::*;
 
-const SHADER_SOURCE: &str = "
+const SDF_SHADER_SOURCE: &str = "
 varying vec2 v_uv;
 
 #ifdef VERTEX_SHADER
@@ -10,11 +10,35 @@ attribute vec2 i_size;
 attribute vec2 i_uv_pos;
 attribute vec2 i_uv_size;
 
-uniform mat3 u_projection_matrix;
-uniform mat3 u_view_matrix;
+uniform mat3 u_matrix;
 void main() {
     v_uv = i_uv_pos + a_pos * i_uv_size;
-    vec3 pos = u_projection_matrix * u_view_matrix * vec3(i_pos + a_pos * i_size, 1.0);
+    vec3 pos = u_matrix * vec3(i_pos + a_pos * i_size, 1.0);
+    gl_Position = vec4(pos.xy, 0.0, pos.z);
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+uniform sampler2D u_texture;
+
+void main() {
+    gl_FragColor = texture2D(u_texture, v_uv);
+}
+#endif
+";
+
+const SHADER_SOURCE: &str = "
+varying vec2 v_uv;
+
+#ifdef VERTEX_SHADER
+attribute vec2 a_pos;
+
+uniform mat3 u_projection_matrix;
+uniform mat3 u_view_matrix;
+uniform mat3 u_model_matrix;
+void main() {
+    v_uv = a_pos;
+    vec3 pos = u_projection_matrix * u_view_matrix * u_model_matrix * vec3(a_pos, 1.0);
     gl_Position = vec4(pos.xy, 0.0, pos.z);
 }
 #endif
@@ -29,18 +53,21 @@ float aa(float edge, float x) {
 
 void main() {
     float dist = (texture2D(u_texture, v_uv).x - 0.5) * 2.0;
-    float w = length(vec2(dFdx(dist), dFdy(dist)));
     float inside = aa(0.0, dist);
     float inside_border = aa(-0.15, dist);
     vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
     vec4 border_color = vec4(1.0, 1.0, 1.0, 1.0);
-    gl_FragColor = color * inside + (1.0 - inside) * (border_color * inside_border + vec4(border_color.xyz, 0.0) * (1.0 - inside_border));
+    vec4 outside_color = vec4(border_color.xyz, 0.0);
+    //vec4 outside_color = vec4(0.0, 1.0, 0.0, 1.0);
+    gl_FragColor = color * inside + (1.0 - inside) * (border_color * inside_border + outside_color * (1.0 - inside_border));
+    gl_FragColor = color * inside + (1.0 - inside) * vec4(border_color.rgb, -dist);
 }
 #endif
 ";
 
 struct State {
     geng: Geng,
+    sdf_program: ugli::Program,
     program: ugli::Program,
     font: geng::font::Ttf,
 }
@@ -59,25 +86,33 @@ impl State {
         Self {
             geng: geng.clone(),
             font,
+            sdf_program: geng.shader_lib().compile(SDF_SHADER_SOURCE).unwrap(),
             program: geng.shader_lib().compile(SHADER_SOURCE).unwrap(),
         }
     }
-}
 
-impl geng::State for State {
-    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
-        ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
-        let text = "� О, аутлайн!";
-        let bb = self.font.measure_bounding_box(text).unwrap();
-        let camera = geng::Camera2d {
-            center: bb.center(),
-            rotation: 0.0,
-            fov: 4.0,
-        };
-        self.font.draw_with(text, |glyphs, atlas| {
+    fn create_text_sdf(
+        &self,
+        text: &str,
+        font: &geng::Font,
+        pixel_size: f32,
+    ) -> Option<ugli::Texture> {
+        let aabb = font.measure_bounding_box(text)?;
+        let texture_size = (vec2(
+            aabb.width() + 2.0 * font.max_distance(),
+            1.0 + 2.0 * font.max_distance(),
+        ) * pixel_size)
+            .map(|x| x.ceil() as usize);
+        let mut texture = ugli::Texture::new_uninitialized(self.geng.ugli(), texture_size);
+        let framebuffer = &mut ugli::Framebuffer::new_color(
+            self.geng.ugli(),
+            ugli::ColorAttachment::Texture(&mut texture),
+        );
+        ugli::clear(framebuffer, Some(Rgba::TRANSPARENT_BLACK), None, None);
+        font.draw_with(text, |glyphs, atlas| {
             ugli::draw(
                 framebuffer,
-                &self.program,
+                &self.sdf_program,
                 ugli::DrawMode::TriangleFan,
                 ugli::instanced(
                     &ugli::VertexBuffer::new_dynamic(
@@ -91,55 +126,66 @@ impl geng::State for State {
                     ),
                     &ugli::VertexBuffer::new_dynamic(self.geng.ugli(), glyphs.to_vec()),
                 ),
-                (
-                    ugli::uniforms! {
-                        u_texture: atlas,
-                    },
-                    geng::camera2d_uniforms(&camera, framebuffer.size().map(|x| x as f32)),
-                ),
+                ugli::uniforms! {
+                    u_texture: atlas,
+                    u_matrix: mat3::ortho(aabb.extend_uniform(font.max_distance())),
+                },
                 ugli::DrawParameters {
-                    blend_mode: Some(ugli::BlendMode::default()),
+                    blend_mode: Some(ugli::BlendMode::combined(ugli::ChannelBlendMode {
+                        src_factor: ugli::BlendFactor::One,
+                        dst_factor: ugli::BlendFactor::One,
+                        equation: ugli::BlendEquation::Max,
+                    })),
                     ..default()
                 },
             );
         });
+        Some(texture)
+    }
+}
 
-        let start = bb.min.x;
-        let end = bb.max.x;
-        let line_width = 0.02;
-        self.geng.draw_2d(
-            framebuffer,
-            &camera,
-            &draw_2d::Segment::new(
-                Segment(vec2(start, 0.0), vec2(end, 0.0)),
-                line_width,
-                Rgba::new(1.0, 0.0, 0.0, 0.5),
-            ),
-        );
-        self.geng.draw_2d(
-            framebuffer,
-            &camera,
-            &draw_2d::Segment::new(
-                Segment(
-                    vec2(start, self.font.ascender()),
-                    vec2(end, self.font.ascender()),
+impl geng::State for State {
+    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
+        let text = "� О, аутлайн!";
+        if let Some(texture) = self.create_text_sdf(text, &self.font, 128.0) {
+            ugli::draw(
+                framebuffer,
+                &self.program,
+                ugli::DrawMode::TriangleFan,
+                &ugli::VertexBuffer::new_dynamic(
+                    self.geng.ugli(),
+                    vec![
+                        draw_2d::Vertex {
+                            a_pos: vec2(0.0, 0.0),
+                        },
+                        draw_2d::Vertex {
+                            a_pos: vec2(1.0, 0.0),
+                        },
+                        draw_2d::Vertex {
+                            a_pos: vec2(1.0, 1.0),
+                        },
+                        draw_2d::Vertex {
+                            a_pos: vec2(0.0, 1.0),
+                        },
+                    ],
                 ),
-                line_width,
-                Rgba::new(0.0, 1.0, 0.0, 0.5),
-            ),
-        );
-        self.geng.draw_2d(
-            framebuffer,
-            &camera,
-            &draw_2d::Segment::new(
-                Segment(
-                    vec2(start, self.font.descender()),
-                    vec2(end, self.font.descender()),
+                (
+                    ugli::uniforms! {
+                        u_model_matrix: mat3::scale(texture.size().map(|x| x as f32)),
+                        u_texture: texture,
+                    },
+                    geng::camera2d_uniforms(
+                        &geng::PixelPerfectCamera,
+                        framebuffer.size().map(|x| x as f32),
+                    ),
                 ),
-                line_width,
-                Rgba::new(0.0, 0.0, 1.0, 0.5),
-            ),
-        );
+                ugli::DrawParameters {
+                    blend_mode: Some(ugli::BlendMode::straight_alpha()),
+                    ..default()
+                },
+            );
+        }
     }
 }
 
