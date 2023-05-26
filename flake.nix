6bedbe5a93ffd70a71ee0e64493a794e1166874e
kuviman <kuviman@gmail.com>
2023-05-26 14:41:18 +0400

    improve flake

--- flake.nix
+++ flake.nix
@@ -10,77 +10,77 @@
   };
   outputs = { self, nixpkgs, rust-overlay, crane, utils }:
     {
-      makeFlakeOutputs = src:
-        utils.lib.eachDefaultSystem (system:
-          let
-            overlays = [ (import rust-overlay) ];
-            pkgs = import nixpkgs { inherit system overlays; };
-            rust-toolchain = pkgs.rust-bin.stable."1.69.0".minimal;
-            crane-lib = crane.lib.${system};
-            waylandDeps = with pkgs; [
-              libxkbcommon
-              wayland
+      makeFlakeSystemOutputs = system: { src, buildInputs ? [ ] }:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          rust-toolchain = pkgs.rust-bin.stable."1.69.0".minimal;
+          crane-lib = crane.lib.${system};
+          waylandDeps = with pkgs; [
+            libxkbcommon
+            wayland
+          ];
+          xorgDeps = with pkgs; [
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ];
+          libDeps = with pkgs; buildInputs
+          ++ waylandDeps ++ xorgDeps ++ [
+            alsa-lib
+            udev
+            libGL
+            xorg.libxcb
+          ];
+          nativeBuildDeps = with pkgs; [ pkg-config ];
+          buildDeps = with pkgs; libDeps ++ [ xorg.libxcb ];
+          libPath = pkgs.lib.makeLibraryPath libDeps;
+          package =
+            let
+              commonArgs = {
+                inherit src;
+                nativeBuildInputs = nativeBuildDeps ++ [ pkgs.makeWrapper ];
+                buildInputs = buildDeps;
+              };
+              package = crane-lib.buildPackage (commonArgs // {
+                cargoArtifacts = crane-lib.buildDepsOnly commonArgs;
+              });
+              finalPackage = package.overrideAttrs (finalAttrs: prevAttrs: {
+                postPhases = [ "copyAssetsPhase" "wrapProgramPhase" ];
+                copyAssetsPhase = ''
+                  cp -r ${src + "/assets"} $out/bin/assets
+                '';
+                wrapProgramPhase = ''
+                  wrapProgram "$out/bin/${finalAttrs.pname}" \
+                    --set WINIT_UNIX_BACKEND x11 \
+                    --prefix LD_LIBRARY_PATH : "${libPath}"
+                '';
+              });
+            in
+            finalPackage;
+        in
+        {
+          defaultPackage = package;
+          defaultApp = utils.lib.mkApp {
+            drv = package;
+          };
+          devShell = with pkgs; mkShell {
+            nativeBuildInputs = nativeBuildDeps;
+            buildInputs = buildDeps ++ [
+              cargo
+              rustPackages.clippy
+              rustfmt
+              rust-analyzer
             ];
-            xorgDeps = with pkgs; [
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXrandr
-            ];
-            libDeps = with pkgs; waylandDeps ++ xorgDeps ++ [
-              alsa-lib
-              udev
-              libGL
-              xorg.libxcb
-            ];
-            nativeBuildDeps = with pkgs; [ pkg-config ];
-            buildDeps = with pkgs; libDeps ++ [ xorg.libxcb ];
-            libPath = pkgs.lib.makeLibraryPath libDeps;
-            package =
-              let
-                commonArgs = {
-                  inherit src;
-                  nativeBuildInputs = nativeBuildDeps ++ [ pkgs.makeWrapper ];
-                  buildInputs = buildDeps;
-                };
-                package = crane-lib.buildPackage (commonArgs // {
-                  cargoArtifacts = crane-lib.buildDepsOnly commonArgs;
-                });
-                finalPackage = package.overrideAttrs (finalAttrs: prevAttrs: {
-                  postPhases = [ "copyAssetsPhase" "wrapProgramPhase" ];
-                  copyAssetsPhase = ''
-                    cp -r ${src + "/assets"} $out/bin/assets
-                  '';
-                  wrapProgramPhase = ''
-                    wrapProgram "$out/bin/${finalAttrs.pname}" \
-                      --set WINIT_UNIX_BACKEND x11 \
-                      --prefix LD_LIBRARY_PATH : "${libPath}"
-                  '';
-                });
-              in
-              finalPackage;
-          in
-          {
-            defaultPackage = package;
-            defaultApp = utils.lib.mkApp {
-              drv = package;
-            };
-            devShell = with pkgs; mkShell {
-              nativeBuildInputs = nativeBuildDeps;
-              buildInputs = buildDeps ++ [
-                cargo
-                rustPackages.clippy
-                rustfmt
-                rust-analyzer
-              ];
-              shellHook = ''
-                export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libPath}"
-                export WINIT_UNIX_BACKEND=x11 # TODO fix
-              '';
-            };
-            formatter = pkgs.nixpkgs-fmt;
-          }
-        );
+            shellHook = ''
+              export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libPath}"
+              export WINIT_UNIX_BACKEND=x11 # TODO fix
+            '';
+          };
+          formatter = pkgs.nixpkgs-fmt;
+        };
+      makeFlakeOutputs = f: utils.lib.eachDefaultSystem (system: self.makeFlakeSystemOutputs system (f system));
     } // utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
