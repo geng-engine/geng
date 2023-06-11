065af0240015395b1dd9b98c6c8214fa94bc12eb
kuviman <kuviman@gmail.com>
2023-06-11 17:54:21 +0400

    Android

--- flake.nix
+++ flake.nix
@@ -7,17 +7,28 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
+    android.url = "github:tadfisher/android-nixpkgs";
   };
-  outputs = { self, nixpkgs, rust-overlay, crane, utils }:
+  outputs = { self, nixpkgs, rust-overlay, crane, android, utils }:
     {
       makeFlakeSystemOutputs = system: { src, buildInputs ? [ ], rust ? { } }:
         let
           overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
+          pkgs = import nixpkgs {
+            inherit system overlays;
+            config = {
+              allowUnfree = true;
+              android_sdk.accept_license = true;
+            };
+          };
           rust-version = ({ version = "latest"; } // rust).version;
           rust-toolchain = pkgs.rust-bin.stable.${rust-version}.default.override
             {
-              targets = [ "wasm32-unknown-unknown" "x86_64-pc-windows-gnu" ];
+              targets = [
+                "wasm32-unknown-unknown"
+                "x86_64-pc-windows-gnu"
+                "aarch64-linux-android"
+              ];
             } // rust;
           crane-lib = (crane.lib.${system}).overrideToolchain rust-toolchain;
           waylandDeps = with pkgs; [
@@ -74,6 +85,30 @@
               src = ./.;
               cargoExtraArgs = "--package cargo-geng";
             };
+            androidsdk = (pkgs.androidenv.composeAndroidPackages {
+              cmdLineToolsVersion = "8.0";
+              toolsVersion = "26.1.1";
+              platformToolsVersion = "34.0.1";
+              buildToolsVersions = [ "30.0.3" ];
+              includeEmulator = false;
+              emulatorVersion = "33.1.6";
+              platformVersions = [ "33" ];
+              includeSources = false;
+              includeSystemImages = false;
+              systemImageTypes = [ "google_apis_playstore" ];
+              abiVersions = [
+                "armeabi-v7a"
+                "arm64-v8a"
+              ];
+              cmakeVersions = [ "3.10.2" ];
+              includeNDK = true;
+              ndkVersions = [ "25.2.9519653" ];
+              useGoogleAPIs = false;
+              useGoogleTVAddOns = false;
+              includeExtras = [
+                # "extras;google;gcm"
+              ];
+            }).androidsdk;
           };
         in
         {
@@ -90,16 +125,23 @@
               lib.cargo-geng
               # wineWowPackages.waylandFull
               # pkgsCross.mingwW64.windows.pthreads
+              cargo-apk
+              lib.androidsdk
+              jre
             ];
             shellHook =
               let
                 libPath = pkgs.lib.makeLibraryPath (libDeps ++ [ pkgsCross.mingwW64.windows.pthreads ]);
+                androidSdkRoot = "${lib.androidsdk}/libexec/android-sdk";
+                androidNdkRoot = "${androidSdkRoot}/ndk-bundle";
               in
               ''
                 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libPath}"
                 export WINIT_UNIX_BACKEND=x11 # TODO fix
                 export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="${pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-gcc"
                 export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER="wine64"
+                export ANDROID_SDK_ROOT="${androidSdkRoot}";
+                export ANDROID_NDK_ROOT="${androidNdkRoot}"; 
               '';
           };
           formatter = pkgs.nixpkgs-fmt;
