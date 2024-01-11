72c351ec989b635750dc70bfc8d8343e4269b5d4
InfiniteCoder <66557254+InfiniteCoder01@users.noreply.github.com>
2024-01-11 20:40:43 +0300

    Revert "Debug"
    This reverts commit 888512ba9b9c41c56f8bdf13c0c04edb1a72bade.

--- crates/ugli/src/uniform/mod.rs
+++ crates/ugli/src/uniform/mod.rs
@@ -89,7 +89,6 @@ impl Uniform for [[f32; 4]; 4] {
 
 impl Uniform for &[[[f32; 2]; 2]] {
     fn apply(&self, gl: &raw::Context, info: &UniformInfo) {
-        panic!("&[[[f32; 2]; 2]]");
         gl.uniform_matrix2fv(
             &info.location,
             self.len() as _,
