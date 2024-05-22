b0c0b90d719a37662562d9174a093332a68b6694
kuviman <kuviman@gmail.com>
2024-05-22 16:29:03 +0400

    Fix example to use the actual crate not dependency

--- crates/web-audio-api/examples/spatial_test.rs
+++ crates/web-audio-api/examples/spatial_test.rs
@@ -1,25 +1,22 @@
-use web_audio_api::{
-    context::{AudioContext, BaseAudioContext},
-    node::{AudioBufferSourceNode, AudioNode, PannerNode},
-};
+use geng_web_audio_api::{self as audio, AudioNode as _};
 
 fn main() {
-    let audio = AudioContext::new(Default::default());
-    let buffer = audio
-        .decode_audio_data_sync(std::io::Cursor::new(
-            std::fs::read("examples/sound/assets/hello.wav").unwrap(),
-        ))
-        .unwrap();
-    let mut source = AudioBufferSourceNode::new(&audio, Default::default());
-    source.set_buffer(buffer);
-    let panner = PannerNode::new(&audio, Default::default());
-    source.connect(&panner);
-    panner.connect(&audio.destination());
+    futures::executor::block_on(async {
+        let audio = audio::AudioContext::new().unwrap();
+        let bytes = std::fs::read("examples/sound/assets/hello.wav").unwrap(); // Dont use std::fs on the web
+        let buffer = audio.decode(bytes).await.unwrap();
+        let mut source = audio::AudioBufferSourceNode::new(&audio);
+        source.set_buffer(buffer);
+        let panner = audio::PannerNode::new(&audio);
+        source.connect(&panner);
+        panner.connect(&audio.destination());
 
-    // No sound, but is sound if these two lines are commented
-    panner.disconnect();
-    panner.connect(&audio.destination());
+        // No sound, but is sound if these two lines are commented
+        panner.disconnect();
+        panner.connect(&audio.destination());
 
-    source.start_at_with_offset(0.0, 0.0);
-    std::thread::sleep(std::time::Duration::from_secs(1));
+        source.start_with_offset(0.0);
+
+        std::thread::sleep(std::time::Duration::from_secs(1)); // Dont sleep the thread on the web
+    });
 }
