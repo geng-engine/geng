05aad5728537c3c7e5f83db1e701fea9675e56f6
kuviman <kuviman@gmail.com>
2024-05-22 16:34:29 +0400

    audio example a bit better

--- crates/geng-audio/examples/spatial_sound.rs
+++ crates/geng-audio/examples/spatial_sound.rs
@@ -3,16 +3,20 @@ use batbox_la::*;
 fn main() {
     futures::executor::block_on(async {
         let audio = geng_audio::Audio::new().unwrap();
-        let buffer = audio
-            .decode(std::fs::read("examples/sound/assets/hello.wav").unwrap())
+        let sound = audio
+            .decode(
+                batbox_file::load_bytes("examples/sound/assets/hello.wav")
+                    .await
+                    .unwrap(),
+            )
             .await
             .unwrap();
         audio
             .listener()
             .set_orientation(vec3(1.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0));
-        let mut effect = buffer.effect();
+        let mut effect = sound.effect();
         effect.set_position(vec3(0.0, 1.0, 0.0));
         effect.play();
-        std::thread::sleep_ms(2000);
+        batbox_time::sleep(batbox_time::Duration::from_secs_f64(2.0)).await;
     });
 }
