d008890c4c4196f8019191ff05c19a54466ab0d7
kuviman <kuviman@gmail.com>
2024-06-12 13:50:41 +0400

    Use web-audio-api crate from separate repo

--- crates/web-audio-api/src/platform/mod.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(target_arch = "wasm32")]
-mod web;
-#[cfg(target_arch = "wasm32")]
-pub use web::*;
-
-#[cfg(not(target_arch = "wasm32"))]
-mod native;
-#[cfg(not(target_arch = "wasm32"))]
-pub use native::*;
