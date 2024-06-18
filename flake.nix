b599f261060f573b9b49fab6ecf0b6b4bbf29049
kuviman <kuviman@gmail.com>
2024-06-18 20:04:44 +0400

    update flake

--- flake.nix
+++ flake.nix
@@ -1,16 +1,30 @@
 {
   inputs = {
-    cargo-geng.url = "github:geng-engine/cargo-geng";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    geng.url = "github:geng-engine/cargo-geng";
+    geng.inputs.nixpkgs.follows = "nixpkgs";
   };
-
-  outputs = { self, cargo-geng }:
-    { inherit (cargo-geng) eachDefaultSystem makeFlakeSystemOutputs; } //
-    cargo-geng.eachDefaultSystem (system:
-      {
-        inherit
-          (cargo-geng.makeFlakeSystemOutputs system { src = ./.; })
-          devShell formatter lib;
-      }
-    );
+  outputs = { geng, nixpkgs, systems, ... }:
+    let
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+      };
+      forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system:
+        let pkgs = pkgsFor system;
+        in f system pkgs);
+    in
+    {
+      devShells = forEachSystem (system: pkgs:
+        {
+          default = geng.lib.mkShell {
+            inherit system;
+            target.linux.enable = true;
+            target.android.enable = true;
+            target.windows.enable = true;
+            target.web.enable = true;
+          };
+        });
+      formatter = forEachSystem (system: pkgs: pkgs.nixpkgs-fmt);
+    };
 }
-  
