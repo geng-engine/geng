08bbd3a6203e5a28b66c8b0ebd35911332b1d854
kuviman <kuviman@gmail.com>
2025-06-06 00:53:35 +0400

    simply

--- crates/bug/src/main.rs
+++ crates/bug/src/main.rs
@@ -300,37 +300,14 @@ fn main() {
         use std::collections::HashMap;
 
         struct Program {
-            pub(crate) cache_key: u64,
             pub(crate) handle: gl::types::GLuint,
-            pub(crate) attributes: HashMap<String, AttributeInfo>,
-            pub(crate) uniforms: HashMap<String, UniformInfo>,
-        }
-
-        #[derive(Debug)]
-        pub struct ActiveInfo {
-            pub name: String,
-            pub size: gl::types::GLint,
-            pub typ: gl::types::GLenum,
-        }
-
-        #[derive(Debug)]
-        pub struct AttributeInfo {
-            pub(crate) location: gl::types::GLuint,
-            #[allow(dead_code)]
-            pub(crate) info: ActiveInfo,
-        }
-
-        #[derive(Debug, Clone)]
-        pub struct UniformInfo {
-            pub(crate) location: gl::types::GLint,
-            pub(crate) name: String,
-            // pub(crate) default: Option<UniformValue>,
         }
 
         impl Drop for Program {
             fn drop(&mut self) {
                 unsafe {
                     gl::DeleteProgram(self.handle);
+                    // gl::UseProgram(0);
                 }
             }
         }
@@ -340,15 +317,7 @@ fn main() {
                 unsafe {
                     let shaders: Vec<&Shader> = shaders.into_iter().collect();
                     let mut program = Program {
-                        cache_key: {
-                            use std::sync::atomic::{AtomicU64, Ordering};
-                            static NEXT: AtomicU64 = AtomicU64::new(0);
-                            NEXT.fetch_add(1, Ordering::SeqCst)
-                        },
-                        // ugli: ugli.clone(),
                         handle: gl::CreateProgram(),
-                        uniforms: HashMap::new(),
-                        attributes: HashMap::new(),
                     };
                     assert!(program.handle != 0);
                     for shader in &shaders {
@@ -365,61 +334,8 @@ fn main() {
                     if link_status == gl::FALSE as _ {
                         panic!("link issue");
                     }
-
-                    // Get attributes
-                    // let attribute_count = gl
-                    //     .get_program_parameter_int(&program.handle, raw::ACTIVE_ATTRIBUTES)
-                    //     as usize;
-                    // for index in 0..attribute_count {
-                    //     let info = gl.get_active_attrib(&program.handle, index as raw::UInt);
-                    //     let name = info.name.clone();
-                    //     let location = gl.get_attrib_location(&program.handle, &name);
-                    //     // TODO: why can't this be an assert?
-                    //     if location >= 0 {
-                    //         program.attributes.insert(
-                    //             name,
-                    //             AttributeInfo {
-                    //                 location: location as raw::UInt,
-                    //                 info,
-                    //             },
-                    //         );
-                    //     }
-                    // }
-
-                    // Get uniforms
-                    // let uniform_count = gl
-                    //     .get_program_parameter_int(&program.handle, raw::ACTIVE_UNIFORMS)
-                    //     as usize;
-                    // for index in 0..uniform_count {
-                    //     let info = gl.get_active_uniform(&program.handle, index as raw::UInt);
-                    //     for index in 0..info.size {
-                    //         let name = match info.size {
-                    //             1 => info.name.clone(),
-                    //             _ => format!("{}[{index}]", info.name.strip_suffix("[0]").unwrap()),
-                    //         };
-                    //         if let Some(location) = gl.get_uniform_location(&program.handle, &name)
-                    //         {
-                    //             let default = UniformValue::get_value(&program, &location, &info);
-                    //             // info!("{:?}", name);
-                    //             program.uniforms.insert(
-                    //                 name.clone(),
-                    //                 UniformInfo {
-                    //                     location,
-                    //                     name,
-                    //                     // info,
-                    //                     default,
-                    //                 },
-                    //             );
-                    //         }
-                    //     }
-                    // }
-
-                    // ugli.debug_check();
                     program
                 }
-            }
-            pub fn uniform_info(&self, name: &str) -> Option<UniformInfo> {
-                self.uniforms.get(name).cloned()
             }
             pub(crate) fn bind(&self) {
                 unsafe {
@@ -458,7 +374,7 @@ fn main() {
                         shader.handle,
                         1,
                         [source.as_ptr() as *const gl::types::GLchar].as_ptr(),
-                        [source.as_bytes().len() as gl::types::GLint].as_ptr(),
+                        [source.len() as gl::types::GLint].as_ptr(),
                     );
                     gl::CompileShader(shader.handle);
                     let mut compile_status = 0_i32;
