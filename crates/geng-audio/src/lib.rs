96ac485a491ca1b8be0b00918b50f08b22bede90
nertsal <nestralnertsal@gmail.com>
2026-06-19 15:08:48 +0300

    timestretching audio (native only)

--- crates/geng-audio/src/lib.rs
+++ crates/geng-audio/src/lib.rs
@@ -146,6 +146,33 @@ impl Audio {
             looped: false,
         })
     }
+
+    pub fn timestretch(
+        &self,
+        samples: Vec<Vec<f32>>,
+        speed_ratio: f32,
+        r#type: SoundType,
+    ) -> anyhow::Result<StreamingSoundEffect> {
+        let worklet = self.inner.context.timestretch(samples, speed_ratio)?;
+        let fade_node = wa::GainNode::new(&self.inner.context);
+        let gain_node = wa::GainNode::new(&self.inner.context);
+        worklet.connect(&fade_node).connect(&gain_node);
+        // .connect(&self.context.inner.master_gain_node);
+        // https://github.com/orottier/web-audio-api-rs/issues/494
+
+        // start immediately TODO later
+        gain_node.connect(&*self.register_type(r#type).lock().unwrap().gain);
+
+        Ok(StreamingSoundEffect {
+            r#type,
+            context: self.clone(),
+            source_node: worklet,
+            fade_node,
+            gain_node,
+            fade_in_times: None,
+            spatial_state: SpatialState::NotSpatial,
+        })
+    }
 }
 
 impl Sound {
@@ -164,7 +191,6 @@ impl Sound {
         SoundEffect {
             r#type,
             context: self.context.clone(),
-            // source_node: SoundNode::Buffer(buffer_node),
             source_node: buffer_node,
             fade_node,
             gain_node,
@@ -212,19 +238,10 @@ impl SoundType {
     }
 }
 
-enum SoundNode {
-    Buffer(wa::AudioBufferSourceNode),
-    // Stream(wa::AudioStreamDestinationNode),
-}
-
-impl SoundNode {
-    pub fn stop_at(&mut self, end_time: f64) {
-        match self {
-            SoundNode::Buffer(node) => node.stop_at(end_time),
-            // SoundNode::Stream(node) => node.sto,
-        }
-    }
-}
+// enum SoundNode {
+//     Buffer(wa::AudioBufferSourceNode),
+//     Worklet(wa::WorkletNode),
+// }
 
 pub struct SoundEffect {
     context: Audio,
@@ -238,36 +255,7 @@ pub struct SoundEffect {
 }
 
 impl SoundEffect {
-    // pub fn stream(chunks: impl IntoIterator<Item = Vec<Vec<f32>>>) -> Self {
-    //     let mut buffer_node = wa::AudioBufferSourceNode::new(&self.context.inner.context);
-    //     buffer_node.set_buffer(self.audio_buffer.clone());
-    //     buffer_node.set_loop(self.looped);
-
-    //     let mut stream = SoundStream {
-
-    //     };
-
-    //     let fade_node = wa::GainNode::new(&self.context.inner.context);
-    //     let gain_node = wa::GainNode::new(&self.context.inner.context);
-    //     buffer_node.connect(&fade_node).connect(&gain_node);
-    //     // .connect(&self.context.inner.master_gain_node);
-    //     // https://github.com/orottier/web-audio-api-rs/issues/494
-    //     SoundEffect {
-    //         r#type,
-    //         context: self.context.clone(),
-    //         source_node: SoundNode::Stream(stream),
-    //         fade_node,
-    //         gain_node,
-    //         fade_in_times: None,
-    //         spatial_state: SpatialState::NotSpatial,
-    //     }
-
-    // }
-
     pub fn set_looped(&mut self, looped: bool) {
-        // if let SoundNode::Buffer(node) = &mut self.source_node {
-        //     node.set_loop(looped);
-        // }
         self.source_node.set_loop(looped);
     }
     pub fn fade_in(&mut self, duration: time::Duration) {
@@ -336,6 +324,15 @@ impl SoundEffect {
         node.connect(&*self.context.register_type(self.r#type).lock().unwrap().gain);
         self.source_node.start_with_offset(offset.as_secs_f64());
     }
+    pub fn play_at_from(&mut self, start_delay: time::Duration, offset: time::Duration) {
+        let node: &dyn wa::AudioNode = match &self.spatial_state {
+            SpatialState::NotSpatial => &self.gain_node,
+            SpatialState::Spatial(panner) => panner,
+        };
+        node.connect(&*self.context.register_type(self.r#type).lock().unwrap().gain);
+        self.source_node
+            .start_at_with_offset(start_delay.as_secs_f64(), offset.as_secs_f64());
+    }
     pub fn set_speed(&mut self, speed: f32) {
         self.source_node.playback_rate().set_value(speed);
     }
@@ -373,5 +370,119 @@ impl SoundEffect {
 }
 
 impl Drop for SoundEffect {
+    fn drop(&mut self) {}
+}
+
+pub struct StreamingSoundEffect {
+    context: Audio,
+    r#type: SoundType,
+    source_node: wa::WorkletNode,
+    gain_node: wa::GainNode,
+    fade_node: wa::GainNode,
+    fade_in_times: Option<std::ops::Range<f64>>,
+    spatial_state: SpatialState,
+}
+
+impl StreamingSoundEffect {
+    pub fn fade_in(&mut self, duration: time::Duration) {
+        let current_time = self.context.inner.context.current_time();
+        let end_time = current_time + duration.as_secs_f64();
+        let fade_gain = self.fade_node.gain();
+        fade_gain.cancel_scheduled_changes(current_time);
+
+        // fade_gain.set_value(0.0);
+        // workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1171438
+        fade_gain.linear_ramp_to_value_at_time(0.0, current_time);
+
+        fade_gain.linear_ramp_to_value_at_time(1.0, end_time);
+
+        self.fade_in_times = Some(current_time..end_time);
+    }
+    pub fn fade_out(&mut self, duration: time::Duration) {
+        let current_time = self.context.inner.context.current_time();
+        let current_value = match self.fade_in_times.take() {
+            Some(times) => {
+                let duration = times.end - times.start;
+                if duration.abs() < 1e-5 {
+                    1.0
+                } else {
+                    ((current_time - times.start) / duration).clamp(0.0, 1.0) as f32
+                }
+            }
+            None => 1.0,
+        };
+        // actual fade out duration will be shorter if fade_out is called shortly after fade_in
+        let fade_out_duration = duration.as_secs_f64() * current_value as f64;
+        let end_time = current_time + fade_out_duration;
+        let fade_gain = self.fade_node.gain();
+        fade_gain.cancel_scheduled_changes(current_time);
+
+        // Like in fade in, working around a bug
+        fade_gain.linear_ramp_to_value_at_time(current_value, current_time);
+
+        fade_gain.linear_ramp_to_value_at_time(0.0, end_time);
+        // self.source_node.stop_at(end_time);
+    }
+
+    pub fn set_volume(&mut self, volume: f32) {
+        self.gain_node.gain().set_value(volume);
+    }
+
+    pub fn fade_to_volume(&mut self, volume: f32, duration: time::Duration) {
+        let current_time = self.context.inner.context.current_time();
+        let end_time = current_time + duration.as_secs_f64();
+        let fade_gain = self.gain_node.gain();
+
+        fade_gain.cancel_scheduled_changes(current_time);
+
+        fade_gain.linear_ramp_to_value_at_time(volume, end_time);
+    }
+
+    // pub fn play(&mut self) {
+    //     let node: &dyn wa::AudioNode = match &self.spatial_state {
+    //         SpatialState::NotSpatial => &self.gain_node,
+    //         SpatialState::Spatial(panner) => panner,
+    //     };
+    //     node.connect(&*self.context.register_type(self.r#type).lock().unwrap().gain);
+    //     self.source_node.start_with_offset(offset.as_secs_f64());
+    // }
+    // pub fn set_speed(&mut self, speed: f32) {
+    //     self.source_node.playback_rate().set_value(speed);
+    // }
+    pub fn stop(&mut self) {
+        self.fade_to_volume(0.0, time::Duration::from_secs_f64(0.001));
+        // self.source_node.stop();
+    }
+    pub fn set_position(&mut self, position: vec3<f32>) {
+        let panner_node = self.make_spatial();
+        panner_node.set_position(**position);
+    }
+    pub fn set_ref_distance(&mut self, distance: f32) {
+        let panner_node = self.make_spatial();
+        panner_node.set_ref_distance(distance as f64);
+    }
+    pub fn set_max_distance(&mut self, max_distance: f32) {
+        let panner_node = self.make_spatial();
+        panner_node.set_max_distance(max_distance as f64);
+    }
+    fn make_spatial(&mut self) -> &mut wa::PannerNode {
+        if let SpatialState::NotSpatial = &self.spatial_state {
+            let mut panner_node = wa::PannerNode::new(&self.context.inner.context);
+            panner_node.set_distance_model(wa::DistanceModel::Linear);
+            self.gain_node.connect(&panner_node);
+            self.spatial_state = SpatialState::Spatial(panner_node);
+        }
+        let SpatialState::Spatial(panner_node) = &mut self.spatial_state else {
+            unreachable!()
+        };
+        panner_node
+    }
+
+    // pub fn playback_position(&self) -> time::Duration {
+    //     time::Duration::from_secs_f64(self.source_node.position())
+    // }
+}
+
+impl Drop for StreamingSoundEffect {
     fn drop(&mut self) {}
 }
